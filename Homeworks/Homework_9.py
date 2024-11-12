b1a57346156f100674007facc3918e0bbc987bd6
by_todorova <b.todorova@mail.com>
2024-11-12 19:35:48 +0200

    First commit for Hw_9

---
+++ Homeworks/Homework_9.py
@@ -0,0 +1,24 @@
+# 1. Create a Python script that reads a text file called "numbers.txt" containing integers and calculates their sum.
+
+
+# 2. Write a program that reads a text file, "words.txt," and counts the number of words in it.
+
+
+# 3. Create a Python script that prompts the user to enter student names and their corresponding scores, then stores this data in a CSV file called "student_scores.csv."
+
+
+# 4. Write a program that reads a CSV file called "employee_data.csv," and for each employee, calculates their total salary (considering base salary and bonuses) and saves it in a new CSV file called "total_salaries.csv."
+
+
+# 5. Design a program that reads a JSON file containing a list of products with names and prices. Calculate the total cost of all products and display it.
+
+
+# 6. Write a Python script that reads a JSON file, "contacts.json," containing contact information (name, email, phone).
+
+
+
+# 7. Create a CSV file, "student_data.csv," with student names and their corresponding JSON data containing exam scores. Write a program that reads the CSV file and calculates the average score for each student.
+
+
+
+# 9. Provide an example XML file, "inventory.xml," that represents a list of products in a store. Write a Python program to read this XML file and print the names and prices of all products
